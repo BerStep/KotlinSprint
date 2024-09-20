34bc56fddd81972e1cf17201fc5a0d14b7f48c24
Stepan <steras23@yandex.ru>
2024-09-20 18:11:29 +0300

    KS-1-5-variables refactor

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,21 +1,8 @@
 package org.example.lesson1
 
 fun main() {
-    val second = 6480
-    val minute = second / 60
-    val hour = minute / 60
+    val second: Int = 6480
+    val minute: Int = second / 60
+    val hour: Int = minute / 60
     println("0$hour:${minute % 60}:0${second % 60}")
-
-    // второй вариант
-    println("")
-    val allSecond = 6480
-    val allMinute = allSecond / 60
-    val allHour = allSecond / 60 / 60
-    print(0)
-    print(allHour)
-    print(":")
-    print(allMinute - allHour * 60)
-    print(":")
-    print(allSecond % 60)
-    print(0)
 }
