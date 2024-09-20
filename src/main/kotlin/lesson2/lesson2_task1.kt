a3e241ee4cf9ab6534d297c010ff7169e5ae7553
Stepan <steras23@yandex.ru>
2024-09-20 18:50:45 +0300

    KS-2-1-math create

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson2
+
+fun main() {
+    val students = 4
+    val mark1 = 3f
+    val mark2 = 4f
+    val mark3 = 3f
+    val mark4 = 5f
+    val avgMark = (mark1 + mark2 + mark3 + mark4) / students
+    println(avgMark)
+}
