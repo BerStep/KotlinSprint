f1ed8c4bbd3725eff4a38615e1071f8ef98054aa
Stepan <steras23@yandex.ru>
2024-09-20 20:02:02 +0300

    KS-2-4-math create

---
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson2
+
+fun main() {
+    val crystalOre = 7
+    val ironOre = 11
+    val buff = 20
+    val buffCrystalOre = crystalOre * buff / 100
+    val buffIronOre = ironOre * buff / 100
+    println(buffCrystalOre)
+    println(buffIronOre)
+}
+
