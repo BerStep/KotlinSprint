e72a1a967f1202cd55755d3fc95fc347df32c069
Stepan <steras23@yandex.ru>
2024-09-21 11:41:38 +0300

    KS-2-4-math refactor

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -4,9 +4,11 @@ fun main() {
     val crystalOre = 7
     val ironOre = 11
     val buff = 20
+
     val buffCrystalOre = crystalOre * buff / 100
     val buffIronOre = ironOre * buff / 100
-    println(buffCrystalOre)
-    println(buffIronOre)
+
+    println("Колличество бонусных  материалов из кристаличесокй руды: $buffCrystalOre")
+    println("Колличество бонусных  материалов из железной руды: $buffIronOre")
 }
 
