47c2df4a124090453f219097ebbc86bb5606fb99
Stepan <steras23@yandex.ru>
2024-09-21 11:38:24 +0300

    KS-2-3-math refactor

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -4,8 +4,10 @@ fun main() {
     val departureHour = 9
     val departureMinutes = 39
     val travelTime = 457
+
     val departureTime = departureHour * 60 + departureMinutes
     val arriveTime = departureTime + travelTime
+
     println("Время прибытия: ${arriveTime / 60}:${arriveTime % 60}")
 }
 
