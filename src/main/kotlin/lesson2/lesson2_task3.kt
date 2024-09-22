8cd68c7a9fe374e0e6d3e14c5fc1811dfe55fb52
Stepan <steras23@yandex.ru>
2024-09-22 23:25:08 +0300

    KS-2-3-math refactor

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -4,10 +4,14 @@ fun main() {
     val departureHour = 9
     val departureMinutes = 39
     val travelTime = 457
+    val converterMinutes = 60
+    val converterHour = 24
 
-    val departureTime = departureHour * 60 + departureMinutes
+    val departureTime = departureHour * converterMinutes + departureMinutes
     val arriveTime = departureTime + travelTime
+    val arriveHour = arriveTime / converterMinutes % converterHour
+    val arriveMinutes = arriveTime % converterMinutes
 
-    println("Время прибытия: ${arriveTime / 60}:${arriveTime % 60}")
+    println("Время прибытия: ${String.format("%02d:%02d", arriveHour, arriveMinutes)}")
 }
 
