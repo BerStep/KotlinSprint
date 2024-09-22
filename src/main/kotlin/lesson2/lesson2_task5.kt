6e8caf22d915a20aa9f168bb0ad03b70a1b5ddac
Stepan <steras23@yandex.ru>
2024-09-22 23:35:39 +0300

    KS-2-5-math refactor

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -7,8 +7,9 @@ fun main() {
     val deposit = 70_000
     val interestRate = 16.7
     val period = 20
+
     val depositBalance = deposit * (1 + interestRate / 100).pow(period)
-    val result = (depositBalance * 1000).roundToInt() / 1000.0
-    println(result)
+
+    println("Размер вклада, черз 20 лет: %.3f".format(depositBalance))
 }
 
