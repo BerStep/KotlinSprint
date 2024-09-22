e86b53a60af79988b596b0ea79aba0a71302d002
Stepan <steras23@yandex.ru>
2024-09-22 23:04:54 +0300

    KS-1-5-math refactor according to the comments

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,13 +1,16 @@
 package org.example.lesson1
 
 fun main() {
-    val converter = 60
+    val converterMinute: Int = 60
+    val converterHour: Int = 24
     var second: Int = 6480
-    var minute: Int = second / converter
-    var hour: Int = minute / converter
-    second %= converter
-    minute %= converter
-    hour %= 24
-    val time = String.format("0%d:%d:0%d", hour, minute, second)
-    println(time)
+
+    var minute: Int = second / converterMinute
+    var hour: Int = minute / converterMinute
+    second %= converterMinute
+    minute %= converterMinute
+    hour %= converterHour
+    val time = String.format("%02d:%d:%02d", hour, minute, second)
+
+    println("Время проведенное в космосе: $time")
 }
