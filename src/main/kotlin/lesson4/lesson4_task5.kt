ccfc4cc43992e98036e52461731ad499e74814b5
Stepan <steras23@yandex.ru>
2024-09-26 23:21:33 +0300

    refactor lesson4_task5.kt

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -6,23 +6,31 @@ const val IS_FAVORABLE_WEATHER: Boolean = true
 
 fun main() {
     println("Есть ли повреждения коробля? (true/false)")
+
     val isDamageOfShip = readlnOrNull().toBoolean()
+
     println("Вы ввели: $isDamageOfShip")
     println("Введите колличество экипажа:")
+
     val crewOfShip = readlnOrNull()?.toIntOrNull()   ?: return println("Не корректные данные")
+
     println("Вы ввели: $crewOfShip")
     println("Введите колличество ящиков с провизией:")
+
     val provisionsShip = readlnOrNull()?.toIntOrNull()   ?: return println("Не корректные данные")
+
     println("Вы ввели: $provisionsShip")
     println("Благоприятная сейчас погода? (true/false)")
+
     val isFavorableWeather = readlnOrNull().toBoolean()
+
     println("Вы ввели: $isFavorableWeather")
 
-    val shipReadiness: Boolean =
+    val isShipReadiness: Boolean =
         ((isDamageOfShip == IS_DAMAGE_SHIP) && (crewOfShip in CREW_OF_SHIP_MIN..CREW_OF_SHIP_MAX) &&
                 (provisionsShip > PROVISIONS_SHIP) && (isFavorableWeather == IS_FAVORABLE_WEATHER)) ||
                 ((crewOfShip == CREW_OF_SHIP_MAX) && (isFavorableWeather == IS_FAVORABLE_WEATHER) &&
                         (provisionsShip > PROVISIONS_SHIP))
 
-    println("Готов ли корабль отправится в плавание?: $shipReadiness")
+    println("Готов ли корабль отправится в плавание?: $isShipReadiness")
 }
