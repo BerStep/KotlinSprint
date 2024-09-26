95f09027470ba99de729787d819dbea6fff78bbd
BerStep <steras23@yandex.ru>
2024-09-26 12:41:38 +0300

    refactor lession4_task5.kt

--- src/main/kotlin/lesson4/lession4_task5.kt
+++ src/main/kotlin/lesson4/lession4_task5.kt
@@ -1,24 +1,28 @@
-const val DAMAGE_SHIP = false
-const val CREW_OF_SHIP_MIN = 55
-const val CREW_OF_SHIP_MAX = 70
-const val PROVISIONS_SHIP = 50
-const val FAVORABLE_WEATHER = true
+const val IS_DAMAGE_SHIP: Boolean = false
+const val CREW_OF_SHIP_MIN: Int = 55
+const val CREW_OF_SHIP_MAX: Int = 70
+const val PROVISIONS_SHIP: Int = 50
+const val IS_FAVORABLE_WEATHER: Boolean = true
 
 fun main() {
-    println("Есть ли повреждения коробля?")
-    val damageOfShip = readlnOrNull().toBoolean()
+    println("Есть ли повреждения коробля? (true/false)")
+    val isDamageOfShip = readlnOrNull().toBoolean()
+    println("Вы ввели: $isDamageOfShip")
     println("Введите колличество экипажа:")
-    val crewOfShip = readlnOrNull()?.toInt()
+    val crewOfShip = readlnOrNull()?.toIntOrNull()   ?: return println("Не корректные данные")
+    println("Вы ввели: $crewOfShip")
     println("Введите колличество ящиков с провизией:")
-    val provisionsShip = readlnOrNull()?.toInt()
-    println("Благоприятная сейчас погода?:")
-    val favorableWeather = readlnOrNull().toBoolean()
+    val provisionsShip = readlnOrNull()?.toIntOrNull()   ?: return println("Не корректные данные")
+    println("Вы ввели: $provisionsShip")
+    println("Благоприятная сейчас погода? (true/false)")
+    val isFavorableWeather = readlnOrNull().toBoolean()
+    println("Вы ввели: $isFavorableWeather")
 
     val shipReadiness: Boolean =
-        ((damageOfShip == DAMAGE_SHIP) && (crewOfShip in CREW_OF_SHIP_MIN..CREW_OF_SHIP_MAX) &&
-                (provisionsShip!! > PROVISIONS_SHIP) && (favorableWeather == FAVORABLE_WEATHER)) ||
-                ((crewOfShip == CREW_OF_SHIP_MAX) && (favorableWeather == FAVORABLE_WEATHER) &&
-                        (provisionsShip!! > PROVISIONS_SHIP))
+        ((isDamageOfShip == IS_DAMAGE_SHIP) && (crewOfShip in CREW_OF_SHIP_MIN..CREW_OF_SHIP_MAX) &&
+                (provisionsShip > PROVISIONS_SHIP) && (isFavorableWeather == IS_FAVORABLE_WEATHER)) ||
+                ((crewOfShip == CREW_OF_SHIP_MAX) && (isFavorableWeather == IS_FAVORABLE_WEATHER) &&
+                        (provisionsShip > PROVISIONS_SHIP))
 
     println("Готов ли корабль отправится в плавание?: $shipReadiness")
 }
