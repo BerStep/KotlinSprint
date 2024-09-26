ee1df175abffd7e1ccaf93ba2c32d5b606d054df
BerStep <steras23@yandex.ru>
2024-09-26 10:33:04 +0300

    refactor lessoon4_task4.kt

--- src/main/kotlin/lesson4/lessoon4_task4.kt
+++ src/main/kotlin/lesson4/lessoon4_task4.kt
@@ -1,15 +1,13 @@
 fun main() {
     val trainingDay = 5
-    val kindTraining = trainingDay % 2
-    val handsAndPress = kindTraining == 1
-    val footAndBack = kindTraining == 0
+    val isSomeSituation = trainingDay % 2 == 0
 
     println(
         """
-            Упражнения для рук: $handsAndPress
-            Упражнения для ног: $footAndBack
-            Упражнения для спины: $footAndBack            
-            Упражнения для пресса: $handsAndPress         
+            Упражнения для рук: ${!isSomeSituation}
+            Упражнения для ног: $isSomeSituation
+            Упражнения для спины: $isSomeSituation
+            Упражнения для пресса: ${!isSomeSituation}
     """.trimIndent()
     )
 }
