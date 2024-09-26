3b1092ccd6163b3a0a84a1fe407103e175bad356
Stepan <steras23@yandex.ru>
2024-09-26 23:10:12 +0300

    lesson4_task4.kt correction variable name

--- src/main/kotlin/lesson4/lessoon4_task4.kt
+++ src/main/kotlin/lesson4/lessoon4_task4.kt
@@ -1,13 +1,13 @@
 fun main() {
     val trainingDay = 5
-    val isSomeSituation = trainingDay % 2 == 0
+    val isEvenDay = trainingDay % 2 == 0
 
     println(
         """
-            Упражнения для рук: ${!isSomeSituation}
-            Упражнения для ног: $isSomeSituation
-            Упражнения для спины: $isSomeSituation
-            Упражнения для пресса: ${!isSomeSituation}
+            Упражнения для рук: ${!isEvenDay}
+            Упражнения для ног: $isEvenDay
+            Упражнения для спины: $isEvenDay
+            Упражнения для пресса: ${!isEvenDay}
     """.trimIndent()
     )
 }
