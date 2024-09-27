9e74bd6a21683845375a31dddc9e81cfa480da4b
Stepan <steras23@yandex.ru>
2024-09-27 08:24:25 +0300

    refactor lesson5_task1.kt

--- src/main/kotlin/lesson5/lesson5_task1.kt
+++ src/main/kotlin/lesson5/lesson5_task1.kt
@@ -1,7 +1,12 @@
 fun main() {
-    val sumResult = 23
-    println("Чему равна сумма двух чисел 15+8?")
+    val verificationName1 = 15
+    val verificationName2 = 8
+    val sumResult = verificationName1 + verificationName2
+
+    println("Чему равна сумма двух чисел $verificationName1+$verificationName2?")
+
     val inputNumber = readlnOrNull()?.toIntOrNull()
+    
     if (inputNumber == sumResult) {
         println("Добро пожаловать!")
     } else println("Доступ запрещен.")
