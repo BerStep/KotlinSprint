b88c0c94fd98dadb412c729f091a9f743215d3e3
Stepan <steras23@yandex.ru>
2024-09-27 08:29:04 +0300

    refactor lesson5_task1.kt

--- src/main/kotlin/lesson5/lesson5_task2.kt
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -2,9 +2,12 @@ const val AGE_OF_MAJORITY = 18
 
 fun main() {
     val currentYear = 2024
+
     println("Введите Ваш год рождения:")
+
     val birthYear = readlnOrNull()?.toIntOrNull() ?: return println("Не корректный ввод.")
     val currentAge = currentYear - birthYear
+
     if (currentAge >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
     else println("Вернуться на главный экран приложения")
 }
