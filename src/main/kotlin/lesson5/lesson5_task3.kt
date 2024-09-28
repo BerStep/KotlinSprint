8e434a01754514b67fca2fb565e98b089257f4a3
Stepan <steras23@yandex.ru>
2024-09-28 14:51:58 +0300

    refactor lesson5_task3.kt

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -10,29 +10,21 @@ fun main() {
     println("Введите два числа от 0 до 42.")
     val userNumberOne = readlnOrNull()?.toIntOrNull() ?: return
     val userNumberTwo = readlnOrNull()?.toIntOrNull() ?: return
-    val userNumberOneReverse = userNumberOne.toString().reversed().toInt()
-    val userNumberTwoReverse = userNumberTwo.toString().reversed().toInt()
 
     println("Числа для победы: $WIN_NUMBER_ONE и $WIN_NUMBER_TWO.")
     println("Учитываются ввод чисел в обтаном порядке.")
 
     if ((userNumberOne) !in range) println("Вы ввели: $userNumberOne, число вне диапозона. ")
-    if (userNumberOneReverse in range) println("Число $userNumberOneReverse ,будет учитываться!")
     if (userNumberTwo !in range) println("Вы ввели: $userNumberTwo, число вне диапозона.")
-    if (userNumberTwoReverse in range) println("Число $userNumberTwoReverse ,будет учитываться!")
      
-    val isWinUserNumber1 = when {
-        userNumberOne == WIN_NUMBER_ONE -> true
-        userNumberOne == WIN_NUMBER_TWO -> true
-        userNumberOneReverse == WIN_NUMBER_ONE -> true
-        userNumberOneReverse == WIN_NUMBER_TWO -> true
+    val isWinUserNumber1 = when (userNumberOne) {
+        WIN_NUMBER_ONE -> true
+        WIN_NUMBER_TWO -> true
         else -> false
     }
-    val isWinUserNumber2 = when {
-        userNumberTwo == WIN_NUMBER_ONE -> true
-        userNumberTwo == WIN_NUMBER_TWO -> true
-        userNumberTwoReverse == WIN_NUMBER_ONE -> true
-        userNumberTwoReverse == WIN_NUMBER_TWO -> true
+    val isWinUserNumber2 = when (userNumberTwo) {
+        WIN_NUMBER_ONE -> true
+        WIN_NUMBER_TWO -> true
         else -> false
     }
 
