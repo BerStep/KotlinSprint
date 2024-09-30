5cf68bb49cc4650c41c75c8f6f989f413117a3dd
Stepan <steras23@yandex.ru>
2024-09-30 08:28:12 +0300

    refactor lesson5_task3.kt cor_random

--- src/main/kotlin/lesson5/lesson5_task3.kt
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -1,8 +1,10 @@
-const val WIN_NUMBER_ONE = 23
-const val WIN_NUMBER_TWO = 36
+//const val WIN_NUMBER_ONE = 23
+//const val WIN_NUMBER_TWO = 36
 
 fun main() {
     val range = 0..42
+    val winNumberOne = (range).random()
+    val winNumberTwo = (range).random()
     val printInfoWinTwoNumbers = "Поздравляем! Вы выйграли главный приз!"
     val printInfoWinOneNumbers = "Вы выйграли утешительный приз!"
     val printInfoNoWin = "Неудача!"
@@ -11,19 +13,19 @@ fun main() {
     val userNumberOne = readlnOrNull()?.toIntOrNull() ?: return
     val userNumberTwo = readlnOrNull()?.toIntOrNull() ?: return
 
-    println("Числа для победы: $WIN_NUMBER_ONE и $WIN_NUMBER_TWO.")
+    println("Числа для победы: $winNumberOne и $winNumberTwo.")
 
     if ((userNumberOne) !in range) println("Вы ввели: $userNumberOne, число вне диапозона. ")
     if (userNumberTwo !in range) println("Вы ввели: $userNumberTwo, число вне диапозона.")
-     
+
     val isWinUserNumber1 = when (userNumberOne) {
-        WIN_NUMBER_ONE -> true
-        WIN_NUMBER_TWO -> true
+        winNumberOne -> true
+        winNumberTwo -> true
         else -> false
     }
     val isWinUserNumber2 = when (userNumberTwo) {
-        WIN_NUMBER_ONE -> true
-        WIN_NUMBER_TWO -> true
+        winNumberOne -> true
+        winNumberTwo -> true
         else -> false
     }
 
